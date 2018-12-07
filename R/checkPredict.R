9c9ee7c62041e2324162765db0e3df399ee44ffa
mbinois <16806814+mbinois@users.noreply.github.com>
2018-12-07 16:57:27 -0600

    default covdist in checkPredict is now "euclidean" for speed

--- R/checkPredict.R
+++ R/checkPredict.R
@@ -4,18 +4,18 @@
 ##' @param x a vector representing the input to check, alternatively a matrix with one point per row,
 ##' @param model list of objects of class \code{\link[DiceKriging]{km}}, one for each objective functions,
 ##' @param threshold optional value for the minimal distance to an existing observation, default to \code{1e-4},
-##' @param distance selection of the distance between new observations, between "\code{euclidean}", "\code{none}",
-##'  "\code{covdist}" (default) and "\code{covratio}", see details,
+##' @param distance selection of the distance between new observations, between "\code{euclidean}" (default), "\code{none}",
+##'  "\code{covdist}" and "\code{covratio}", see details,
 ##' @param type "\code{SK}" or "\code{UK}" (default), depending whether uncertainty related to trend estimation has to be taken into account.
 ##' @details If the distance between \code{x} and the closest observations in \code{model} is below
 ##'  \code{threshold}, \code{x} should not be evaluated to avoid numerical instabilities.
-##' The distance can simply be the Euclidean distance or the canonical distance associated with the kriging covariance k: 
+##' The distance can simply be the Euclidean distance or the canonical distance associated with the kriging predictive covariance k: 
 ##'   \deqn{d(x,y) = \sqrt{k(x,x) - 2k(x,y) + k(y,y)}.}{d(x,y) = \sqrt(k(x,x) - 2k(x,y) + k(y,y)).} 
 ##'   The last solution is the ratio between the prediction variance at \code{x} and the variance of the process.
 ##'   \code{none} can be used, e.g., if points have been selected already.
 ##' @return \code{TRUE} if the point should not be tested.
 ##' @export
-checkPredict <- function(x, model, threshold = 1e-4, distance = "covdist", type = "UK"){
+checkPredict <- function(x, model, threshold = 1e-4, distance = "euclidean", type = "UK"){
   
   if(is.null(dim(x))){
     x <- matrix(x, nrow = 1) 
