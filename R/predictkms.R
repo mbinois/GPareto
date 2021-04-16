c4aeb80d6c99900bdae91c13b678049612cdaac5
Mickael Binois <mickael.binois@inria.fr>
2021-04-16 17:48:45 +0200

    add support for cov.compute = TRUE in predict_kms

--- R/predictkms.R
+++ R/predictkms.R
@@ -1,14 +1,19 @@
-##' Predict function for list of \code{\link[DiceKriging]{km}} models.
-##' @param model list of \code{\link[DiceKriging]{km}} models
-##' @param newdata,type,se.compute,cov.compute,light.return,bias.correct,checkNames,... see \code{\link[DiceKriging]{predict.km}}
-##' @export 
-##' @details So far only \code{light.return = TRUE} and \code{cov.compute = FALSE} handled.
+#' Predict function for list of \code{\link[DiceKriging]{km}} models.
+#' @param model list of \code{\link[DiceKriging]{km}} models
+#' @param newdata,type,se.compute,cov.compute,light.return,bias.correct,checkNames,... see \code{\link[DiceKriging]{predict.km}}
+#' @export 
+#' @details So far only \code{light.return = TRUE} handled.
 predict_kms <- function(model, newdata, type, se.compute = TRUE, 
-                       cov.compute = FALSE, light.return = FALSE,
-                       bias.correct = FALSE, checkNames = TRUE, ...){
+                        cov.compute = FALSE, light.return = TRUE,
+                        bias.correct = FALSE, checkNames = FALSE, ...){
   
-  pred <- lapply(model, FUN=predict, newdata=newdata, checkNames=FALSE, type=type, cov.compute = FALSE, light.return = TRUE)
+  pred <- lapply(model, FUN=predict, newdata=newdata, checkNames=checkNames, type=type, cov.compute = cov.compute, light.return = light.return)
   
-  return(list(mean =  Reduce(rbind, lapply(pred, function(alist) alist$mean)), sd = Reduce(rbind, lapply(pred, function(alist) alist$sd))))
+  if(cov.compute)
+    return(list(mean =  Reduce(rbind, lapply(pred, function(alist) alist$mean)),
+                sd = Reduce(rbind, lapply(pred, function(alist) alist$sd)),
+                cov = lapply(pred, function(alist) alist$cov)))
+  return(list(mean =  Reduce(rbind, lapply(pred, function(alist) alist$mean)),
+              sd = Reduce(rbind, lapply(pred, function(alist) alist$sd))))
 }
 
